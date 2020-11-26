294b417fceca96a52c4440179abf9869c1085b7b
root <root@DESKTOP-3KULAHD.localdomain>
2020-11-25 20:49:21 -0500

    Second game demo version

---
+++ BreakfastBreakoutDemo1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    private float zoom;
+    private float yFix;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y - playerTransform.position.y <= -3)
+        {
+            yFix += 0.05f;
+        }
+        if(transform.position.y - playerTransform.position.y >= 3)
+        {
+            yFix -= 0.05f;
+        }
+        transform.position = new Vector3(playerTransform.position.x,yFix,-30);
+    }
+}
